8e4ca0559298a494c13d4acaf0b4b6c5e7309a67
Chris Vaughan <chris@cevsystems.co.uk>
2025-11-28 10:57:19 +0000

    Minor fix

--- accesspoints.php
+++ accesspoints.php
@@ -4,8 +4,9 @@
  * List of access points for meeting/start locations
  * 
  * task.php
- * get.php  easting=564400&northing=219900&dist=10&maxpoints=10
+ * get.php?easting=564400&northing=219900&dist=10&maxpoints=10
  * getall.php
  * report.php?gr=SK123456&type=like
  * report.php?gr=SK123456&type=dislike
+ * details.php?gr=SK123456 
 */
